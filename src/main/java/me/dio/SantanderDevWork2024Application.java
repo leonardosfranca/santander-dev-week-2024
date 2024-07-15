45491e596e94a588ed023078450392ea9b8fabd0
leonardo franca <leonardo5155@gmail.com>
2024-07-15 09:56:09 -0300

    first commit

---
+++ src/main/java/me/dio/SantanderDevWork2024Application.java
@@ -0,0 +1,13 @@
+package me.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderDevWork2024Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SantanderDevWork2024Application.class, args);
+    }
+
+}
